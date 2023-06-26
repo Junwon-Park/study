54acbd8ea886c8d090ad886d68a7785b4c7ad37e
Jackson <upjw0429@gmail.com>
2023-06-26 22:55:24 +0900

    Upt. MVC 템플릿 엔진을 사용한 방식 정리 완료

--- springboot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ springboot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,14 @@ public class HelloController {
         // 여기에서는 data1이라는 이름으로 Spring!!이라는 값을 전달했다.
         return "hello"; // 여기에서 반환한 값으로 Spring boot는 resource/templates 하위에 있는 반환 값과 동일한 이름을 가진 .html 파일을 찾는다.
         // 해당 파일을 찾으면 위에서 Model에 전달한 data1: "Spring!!"이라는 데이터를 해당 정적 파일(템플릿 엔진)에 전달한다.
+        // 이 때, resource/template에서 리턴 값인 "hello"라는 이름을 가진 html 파일을 찾는 것이 viewResovler라는 것이다. Controller는 Model에 데이터를 담아 텍스트와 함께 반환하고 viewResolver는 Controller에서 반환한 텍스트와 동일한 이름을 가진 html 파일을 찾아
+        // Controller에서 Model에 넣은 데이터와 함께 템플릿 엔진 변환 처리를 해서 Tomcat 서버에 전달하면 Tomcat 서버는 브라우저에 해당 화면을 응답한다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
