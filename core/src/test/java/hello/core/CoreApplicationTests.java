87c021808134fddfdf4b05849c28b67cffbba500
Jackson <upjw0429@gmail.com>
2023-08-01 00:37:12 +0900

    Add. 할인 정책과 주문 단위 테스트 작성

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,6 +3,9 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// 이 테스트는 Spring에서 자동으로 생성해주는 테스트 클래스이다.
+// 하지만 이 테스트는 실행하려면 스프링을 우선 실행해야 하므로 시간이 오래 걸리고 테스트 자체가 무거워진다.
+// 그래서 MemberServiceTest와 OrderServiceTest와 같이 순수 자바만을 사용해서 단위 테스트를 작성해서 테스트하는 것이 더 성능이 좋다.
 @SpringBootTest
 class CoreApplicationTests {
 
---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// 아래 OrderServiceTest의 테스트 처럼 Spring의 테스트를 사용하지 않고 순수 자바 코드로만 테스트하는 것을 단위 테스트라고 한다.
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
