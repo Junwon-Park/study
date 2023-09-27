e000ec05a8a412928932d229c05adebd8693854f
Jackson <upjw0429@gmail.com>
2023-09-27 22:55:05 +0900

    Add. HelloController와 application.yml에 설명 추가

--- springboot/jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
+        // 여기서의 Model 인터페이스는 MVC의 Model이며, 데이터를 View로 넘겨주는 기능(메서드)을 가지고 있다.
         model.addAttribute("data", "hello!!!");
-        return "hello"; // templates 디렉토리 내에서 여기에서 반환하는 문자열과 동일한 이름을 가진 html 파일에 위에서 생성한 model을 전달한다.
+        return "hello"; // templates 디렉토리 내에서 여기에서 반환하는 문자열과 동일한 이름을 가진 html(hello.html) 파일에 위에서 생성한 model을 전달한다.
+        // 스프링 부트에 Thymeleaf를 설치하면 자동으로 스프링 부트의 설정에서 resources:templetes/ +{viewName}+ .html을 자동으로 세팅하고 이대로 실행한다.
+        // 기본 설정은 templetes 디렉토리가 Root 경로로 되어 있지만 이 경로를 변경해서 다른 경로를 지정할 수 있다.(스프링 부트 메뉴얼 참조, 하지만 기본 설정 권장)
     }
 }
--- springboot/jpashop/src/main/java/jpabook/jpashop/Member.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -6,10 +6,11 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Getter @Setter
+@Entity // JPA Entity 지정
+@Getter @Setter // Lombok 사용
 public class Member {
-    @Id @GeneratedValue
+    @Id @GeneratedValue // @Id 애노테이션은 이 필드를 Id 값으로 매핑하도록 지정한 것이고, @GeneratedValue 애노테이션은 데이터 생성 시 이 값을 자동으로 생성하도록 지정한 것이다.
     private Long id;
+
     private String username;
 }
