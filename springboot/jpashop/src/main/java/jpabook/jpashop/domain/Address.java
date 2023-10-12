25a1464ddbbdfef117ce7ca1ce3bd2adde4653dd
Jackson <upjw0429@gmail.com>
2023-10-13 01:12:49 +0900

    Add. 내장 타입 설명 추가

--- springboot/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,18 @@ import lombok.Getter;
 // 그래서 Getter만 열어 두고 Setter는 닫아 둔 것이다.
 public class Address {
 
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() { // JPA 스펙상 엔티티와 임베디드 타입은 자바 기본 생성자를 public 또는 protected로 설정해서 생성해야 한다.
+        // JPA가 자바 기본 생성자를 생성하도록 하는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 할 때 필요하기 때문이다.
+        // 하지만 public으로 열어두는 것 보다는 protected 정도로 좁혀두는 것이 더 안전하다. 즉, protected를 사용하는 것이 권장 사항이다.
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
