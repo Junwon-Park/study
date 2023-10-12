06ebe9c85e0728d8c7047add10eea6498989bec1
Jackson <upjw0429@gmail.com>
2023-10-13 00:15:18 +0900

    Fix. 오타 수정

--- springboot/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -9,6 +9,6 @@ import lombok.Setter;
 @DiscriminatorValue("M") // 이 클래스가 상속받고 있는 부모 클래스(Item)의 @DiscrimitatorColumn에 지정된 필드(DTYPE)의 값으로 저장될 이름을 지정하는 애노테이션이다.
 @Getter @Setter
 public class Movie extends Item {
-    private String dirctor;
+    private String director;
     private String actor;
 }
