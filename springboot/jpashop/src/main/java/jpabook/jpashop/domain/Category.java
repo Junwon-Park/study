91abef1b442e21ea2f0766642c7109911cb31f68
Jackson <upjw0429@gmail.com>
2023-10-15 23:58:53 +0900

    Add. Category 엔티티의 items 필드에 N : N 관련 코멘트 추가

--- springboot/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -17,6 +17,7 @@ public class Category {
     private int price;
     private int stockQuantity;
 
+    // 아래 JoinTable() 애노테이션과 내부 옵션은 N : N을 구현하는 방법이다. 하지만 N : N 방식은 시용하지 않는 것이 좋다.
     @ManyToMany
     @JoinTable(name = "category_item", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
