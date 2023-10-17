74bf79f0db7d9c9989ce8fec70fe5cba861eadf7
Jackson <upjw0429@gmail.com>
2023-10-18 00:10:46 +0900

    Add. 회원 서비스 개발

--- springboot/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,16 +1,21 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor // 엔티티 매니저도 Lombok의 해당 애노테이션을 사용해서 생성자 주입과 동일하게 동작하도록 해서 엔티티 매니저를 주입받도록 할 수 있다.
+// 그러면 @PersistenceContext 애노테이션을 생략할 수 있고 생성자도 생략할 수 있다.
+// 엔티티 매니저 타입의 final 상수만 있으면 된다.
+// 원래 생성자 주입은 변수에 @AutoWired 애노테이션을 붙여야 하지만 엔티티 매니저에는 주입을 위해 @PersistenceContext 애노테이션만 붙일 수 있다.
+// 하지만 JPA에서 @PersistenceContext가 아닌 @AutoWired 애노테이션을 붙여 생성자 주입이 가능하도록 지원해주고 있기 때문에 그 것을 Lombok의 @RequiredArgsConstructor을 사용해 생성자 주입 코드를 작성하지 않고 생성자 주입이 되도록 할 수 있는 것이다.
+// 그래서 최종적으로 아래 코드만으로 동작하도록 할 수 있게 된 것이다.
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
---
+++ springboot/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // DB의 데이터를 변경하는 로직이 있는 경우 반드시 @Transactional 애노테이션을 달아주어야 한다.
+// @Transactional 애노테이션은 spring.transaction.annotation과 jakarta.transaction 두 가지가 있는데, spring.transaction.annotation이 기능이 더 많기 때문에 이 것을 사용하는 것이 좋다.
+// readOnly를 true로 설정하면 이 엔티티의 모든 메서드에 readOnly 옵션이 적용되어 읽기만 가능한 기능이 된다.
+// readOnly 옵션을 true로 하면 내부적으로 조회만 가능하다는 것을 지정한 것이기 때문에 데이터를 변경할 때 영속성 컨텍스트에서 더티 체크를 수행하지 않고 DB에 따라 추가적인 조회에 한해 성능에 이점이 있게 된다.
+// @Transactional의 readOnly 기본 값은 false이다. 그래서 회원 조회와 같은 조회 메서드에는 이 옵션을 true로 하고 데이터를 변경하는 메서드는 false로 해주면 된다.
+@RequiredArgsConstructor // Lombok 라이브러리에서 제공하는 애노테이션이다.
+// final로 선언된 DI 변수에 자동으로 생성자 주입을 해주는 애노테이션이다.
+// !!! 반드시 final로 선언된 DI 상수에만 주입해준다.
+// 그러면 해당 DI 상수에 값을 넣어줄 @AutoWired 생성자가 필요 없어지게 된다.
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // ! Lombok의 @RequiredArgsConstructor를 사용했기 때문에 아래 코드를 작성하지 않아도 자동으로 위 DI 변수에 해당 빈을 주입해준다.
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    // 회원가입
+    @Transactional // 클래스 레벨에서 @Transactional의 readOnly를 true로 주었기 때문에 모든 메서드에 readOnly = true로 동작하게 되는데,
+    // 회원가입 메서드는 데이터를 추가하는 메서드이기 때문에 readOnly를 true로 하면 조회만 가능하게 되기 때문에 데이터 저장이 수행되지 않는다.
+    // 그래서 이 경우 readOnly 옵션을 false로 줘야 하는데, 클래스 레벨에서 준 옵션과 다른 옵션을 주고싶을 때는 해당 메서드에만 추가로 @Transactional 애노테이션을 달아주면 되고,
+    // 기본 값이 readOnly = false이기 때문에 여기에서는 기본 값으로 설정되도록 하면 된다.
+    // 당연히 반대로도 가능하기 때문에 클래스 레벨에서 @Transactional(readOnly 기본 값 false)로 했다면 모든 메서드에 readOnly 옵션 값이 false로 지정된 것이기 때문에 추가로 애노테이션을 붙이지 않아도 되고,
+    // 이 때 단순 조회 메서드에서는 @Transactional(readOnly = true)를 각각 달아줘야 한다.
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원이 있는지 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findAll();
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
