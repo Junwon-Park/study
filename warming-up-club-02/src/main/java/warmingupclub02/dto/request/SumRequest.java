bdef598bbb41fcb058d140b4dc5ef10744e46753
Jackson <upjw0429@gmail.com>
2024-02-21 21:43:40 +0900

    Add. 인프런 워밍업 클럽 GET, POST API

--- warming-up-club-02/src/main/java/warmingupclub02/dto/request/SumRequest.java
+++ warming-up-club-02/src/main/java/warmingupclub02/dto/request/SumRequest.java
@@ -3,6 +3,12 @@ package warmingupclub02.dto.request;
 public class SumRequest {
     int[] numbers;
 
+
+
+    public SumRequest(int[] numbers) {
+        this.numbers = numbers;
+    }
+
     public int[] getNumbers() {
         return numbers;
     }
