8e282f385d37e0baf3087c790015a1c33b0018f0
Jackson <upjw0429@gmail.com>
2024-03-27 20:47:27 +0900

    Add. 특강 신청 과제 프로젝트 생성

---
+++ apply-lecture/src/main/java/homework/applylecture/ApplyLectureApplication.java
@@ -0,0 +1,13 @@
+package homework.applylecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyLectureApplication.class, args);
+    }
+
+}
---
+++ apply-lecture/src/test/java/homework/applylecture/ApplyLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.applylecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
